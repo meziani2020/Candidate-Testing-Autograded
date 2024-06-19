83ea1fe855cadbfdc04489232fade538f70da7cc
Mohamed <issine14@gmail.com>
2024-06-19 14:29:43 -0500

    Task1 work

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName="";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question="Who was the first American woman in space? ";
+let correctAnswer="Sally Ride";
+let candidateAnswer="";
 
 
 //TODO: Variables for Part 2
@@ -18,19 +18,28 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName=input.question("Candidate's Name?");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  candidateAnswer=input.question(question);
 }
-
+/*
+//---my code---
+function askQuestion(Qid) {
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+  if(Qid==="name"){ askForName()}
+}
+*/
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  if( candidateAnswer===correctAnswer){
+      console.log("Good job!, your answer is correct. ");
+  }else{
+    console.log("Oops!, your answer is not correct. ");
+  }
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -41,12 +50,19 @@ function gradeQuiz(candidateAnswers) {
 
 function runProgram() {
   askForName();
+  // ---my code--greatCandidate(candidateName);
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+  console.log("Hello "+candidateName+" !");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
 
+/* 
+//---my code---
+function greatCandidate(C_Name){
+  console.log("Hello "+C_Name+" !");
+}
+*/
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
@@ -59,3 +75,5 @@ module.exports = {
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
 };
+
+runProgram();
