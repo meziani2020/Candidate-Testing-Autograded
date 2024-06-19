b84557c2691c61a8c2c57983645f1eab1f61cc43
Mohamed <issine14@gmail.com>
2024-06-19 18:02:59 -0500

    candidat graded testing  tast2

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,25 @@ let candidateAnswer="";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions=Array();
+let correctAnswers= Array();
+let candidateAnswers=Array();
+ 
+// questions array initializing
+questions[0]= "Who was the first American woman in space? ";
+questions[1]= "True or false: 5 kilometer == 5000 meters? ";
+questions[2]= "(5 + 3)/2 * 10 = ? ";
+questions[3]= "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ";
+questions[4]= "What is the minimum crew size for the ISS? ";
+
+
+//correctAnswers array initializing
+correctAnswers[0]="Sally Ride";
+correctAnswers[1]="true";
+correctAnswers[2]="40";
+correctAnswers[3]="Trajectory";
+correctAnswers[4]="3";
+
 
 
 function askForName() {
@@ -23,24 +39,35 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer=input.question(question);
-}
-/*
-//---my code---
-function askQuestion(Qid) {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  if(Qid==="name"){ askForName()}
-}
-*/
-function gradeQuiz(candidateAnswers) {
+  //candidateAnswer=input.question(question);
 
+  for(let i=0; i<questions.length;i++){
+    candidateAnswers[i]=input.question(questions[i]);
+  }
+
+  
+}
+
+function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  /*
   if( candidateAnswer===correctAnswer){
       console.log("Good job!, your answer is correct. ");
   }else{
     console.log("Oops!, your answer is not correct. ");
   }
+  */
+  
+  let  textTemplat="";
+  //textTemplat=`Your answer: ${candidateAnswers[0]}, Correct answer: ${correctAnswers[0]}.  \n`;
+  //textTemplat=`Your answer: ${t[0]}, Correct answer: ${t[1]}.  \n`;
 
+
+  for(let i=0; i<questions.length; i++){
+    textTemplat += `Your answer: ${candidateAnswers[i]}, Correct answer: ${correctAnswers[i]}.  \n`;
+  }
+  console.log(textTemplat);
+  
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
@@ -48,21 +75,22 @@ function gradeQuiz(candidateAnswers) {
   return grade;
 }
 
+
 function runProgram() {
   askForName();
-  // ---my code--greatCandidate(candidateName);
   // TODO 1.1c: Greet candidate using their name //
   console.log("Hello "+candidateName+" !");
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+  //gradeQuiz(this.candidateAnswers);
+  gradeQuiz(candidateAnswers);
 }
 
-/* 
-//---my code---
-function greatCandidate(C_Name){
-  console.log("Hello "+C_Name+" !");
-}
-*/
+runProgram();
+
+
+
+
+
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
@@ -76,4 +104,3 @@ module.exports = {
   runProgram: runProgram
 };
 
-runProgram();
